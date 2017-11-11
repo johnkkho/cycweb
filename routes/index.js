8b7bb9cc85671f8698ef6dc1361a3f578822b3d4
John Ho <john-kkho@hotmail.com>
2017-11-12 00:40:36 +1100

    removed pages.js and updated index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. */ /* GET Static Pages */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+    res.render('pages/about', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
+});
+
+router.get('/contact', function(req, res, next) {
+    res.render('pages/contact', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
+});
+
+router.get('/insurance', function(req, res, next) {
+    res.render('pages/insurance', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
+});
+
+router.get('/terms', function(req, res, next) {
+    res.render('pages/terms', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/signup', function(req, res, next) {
+    res.render('user/signup', {title: '注冊帳戶', navBar: 0});
+});
+
+router.get('/profile', function(req, res, next) {
+    res.render('user/profile', {title: '我的帳戶', navBar: 0});
 });
 
 module.exports = router;
