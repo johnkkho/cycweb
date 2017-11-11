126b4667a224f326db22e8b1604ce284fa81c27d
John Ho <john-kkho@hotmail.com>
2017-11-12 00:57:03 +1100

    fixed url issue

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,23 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/about', function(req, res, next) {
-    res.render('pages/about', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
+    res.render('about', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
 });
 
 router.get('/contact', function(req, res, next) {
-    res.render('pages/contact', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
+    res.render('contact', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
 });
 
 router.get('/insurance', function(req, res, next) {
-    res.render('pages/insurance', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
+    res.render('insurance', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
 });
 
 router.get('/terms', function(req, res, next) {
-    res.render('pages/terms', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
+    res.render('terms', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
+});
+
+router.get('/gallery', function(req, res, next) {
+    res.render('gallery', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
 });
 
 module.exports = router;
--- routes/shops.js
+++ routes/shops.js
@@ -1,2 +1,41 @@
 var express = require('express');
 var router = express.Router();
+
+/* GET shops Pages. */
+router.get('/attractions', function(req, res, next) {
+    res.render('shop/attraction', { title: 'Shopping Cart', setLayout: 1, navBar: 1 });
+});
+
+router.get('/deals', function(req, res, next) {
+    res.render('shop/deals', { title: 'Shopping Cart', setLayout: 1, navBar: 1 });
+});
+
+router.get('/hotels', function(req, res, next) {
+    res.render('shop/hotels', { title: 'Shopping Cart', setLayout: 1, navBar: 1 });
+});
+
+router.get('/services', function(req, res, next) {
+    res.render('shop/services', { title: 'Shopping Cart', setLayout: 1, navBar: 1 });
+});
+
+router.get('/gallery', function(req, res, next) {
+    res.render('shop/gallery', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
+});
+
+router.get('/products', function(req, res, next) {
+    res.render('shop/products', { title: 'Shopping Cart', setLayout: 1, navBar: 1 });
+});
+
+router.get('/product_details', function(req, res, next) {
+    res.render('shop/product_details', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
+});
+
+router.get('/hotel_details', function(req, res, next) {
+    res.render('shop/hotel_details', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
+});
+
+router.get('/service_details', function(req, res, next) {
+    res.render('shop/service_details', { title: 'Shopping Cart', setLayout: 1, navBar: 0 });
+});
+
+module.exports = router;
