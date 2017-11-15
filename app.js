80c416db5d1ef498c19b841bd723982eb6256222
John Ho <john-kkho@hotmail.com>
2017-11-16 10:17:31 +1100

    added admin route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var hbs = require('express-handlebars');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var shops = require('./routes/shops');
+var admin = require('./routes/admin');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/shops', shops);
+app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
